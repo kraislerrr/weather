674a7eb88e5fded3cf5ffdc355ac8bade6114644
Andrey Pavlushov <97114305+kraislerrr@users.noreply.github.com>
2025-02-26 14:03:38 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.weather
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
@@ -10,10 +12,19 @@ import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+    //private lateinit var mySpinner: Spinner
+
+    companion object {
+        private const val PREFS_NAME = "MyPrefs"
+        private const val KEY_SPINNER_POSITION = "spinner_position"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
 
         val webViewya: WebView = findViewById(R.id.webViewya)
         val webView: WebView = findViewById(R.id.webView)
@@ -26,21 +37,21 @@ class MainActivity : AppCompatActivity() {
 
         spinner.adapter = adapter
 
+        val savedPosition = sharedPreferences.getInt(KEY_SPINNER_POSITION, 0)
+        spinner.setSelection(savedPosition)
+
         webView.settings.javaScriptEnabled = true
         webViewrp.settings.javaScriptEnabled = true
         webViewya.settings.javaScriptEnabled = true
 
         webViewya.settings.userAgentString = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.4) Gecko/20100101 Firefox/4.0";
-        //webViewya.setInitialScale(140);
         webViewya.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
-                //val scriptp = "document.getElementsByClassName('weekDay')[0].scrollIntoView();"
                 val scriptp = "document.getElementsByClassName('fact__props')[0].scrollIntoView();"
                 webViewya.evaluateJavascript(scriptp, null)
             }}
         webViewrp.settings.userAgentString = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.4) Gecko/20100101 Firefox/4.0";
-        //webViewrp.setInitialScale(120);
         webViewrp.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
@@ -55,16 +66,19 @@ class MainActivity : AppCompatActivity() {
                         webView.loadUrl("https://earth.nullschool.net/ru/#current/wind/surface/level/orthographic=-319.26,43.40,2923/loc=40.323,43.296")
                         webViewrp.loadUrl("https://rp5.ru/%D0%90%D1%80%D1%85%D0%B8%D0%B2_%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D1%8B_%D0%B2_%D0%A1%D0%BE%D1%87%D0%B8,_%D0%B8%D0%BC._%D0%92._%D0%98._%D0%A1%D0%B5%D0%B2%D0%B0%D1%81%D1%82%D1%8C%D1%8F%D0%BD%D0%BE%D0%B2%D0%B0_(%D0%B0%D1%8D%D1%80%D0%BE%D0%BF%D0%BE%D1%80%D1%82),_METAR")
                         webViewya.loadUrl("https://dzen.ru/pogoda/sochi?lat=43.585475&lon=39.723095")
+                        sharedPreferences.edit().putInt(KEY_SPINNER_POSITION, position).apply()
                     }
                     2 -> {
                         webView.loadUrl("https://earth.nullschool.net/ru/#current/wind/surface/level/orthographic=-318.48,44.78,2537/loc=41.543,44.513")
                         webViewrp.loadUrl("https://rp5.ru/%D0%90%D1%80%D1%85%D0%B8%D0%B2_%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D1%8B_%D0%B2_%D0%A1%D1%82%D0%B0%D0%B2%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B5,_%D0%B8%D0%BC._%D0%90._%D0%92._%D0%A1%D1%83%D0%B2%D0%BE%D1%80%D0%BE%D0%B2%D0%B0_(%D0%B0%D1%8D%D1%80%D0%BE%D0%BF%D0%BE%D1%80%D1%82),_METAR")
                         webViewya.loadUrl("https://dzen.ru/pogoda/?lat=45.04331589&lon=41.96910858")
+                        sharedPreferences.edit().putInt(KEY_SPINNER_POSITION, position).apply()
                     }
                     3 -> {
                         webView.loadUrl("https://earth.nullschool.net/ru/#current/wind/surface/level/orthographic=-330.17,59.10,2042/loc=30.271,59.934")
                         webViewrp.loadUrl("https://rp5.ru/%D0%90%D1%80%D1%85%D0%B8%D0%B2_%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D1%8B_%D0%B2_%D0%9F%D1%83%D0%BB%D0%BA%D0%BE%D0%B2%D0%BE_(%D0%B0%D1%8D%D1%80%D0%BE%D0%BF%D0%BE%D1%80%D1%82),_METAR")
                         webViewya.loadUrl("https://dzen.ru/pogoda/2?via=moc")
+                        sharedPreferences.edit().putInt(KEY_SPINNER_POSITION, position).apply()
                     }
                     else -> {
                         webView.loadUrl("https://earth.nullschool.net/ru/#current/wind/surface/level/orthographic=-321.59,44.81,5146/loc=37.310,44.892")
